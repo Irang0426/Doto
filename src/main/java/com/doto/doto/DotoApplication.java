52780fc1dd0e460d3d42c097dcc6498754ed0dad
irang <irang0426@gmail.com>
2025-05-21 15:26:18 +0900

    Initial commit

---
+++ src/main/java/com/doto/doto/DotoApplication.java
@@ -0,0 +1,13 @@
+package com.doto.doto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoApplication.class, args);
+	}
+
+}
