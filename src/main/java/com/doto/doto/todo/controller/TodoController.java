672e009611541d4950342f5ba81346c2c23caf01
irang <irang0426@gmail.com>
2025-05-28 16:25:05 +0900

    feat: calendar에서 새 작업 추가 기능 구현

--- src/main/java/com/doto/doto/todo/controller/TodoController.java
+++ src/main/java/com/doto/doto/todo/controller/TodoController.java
@@ -5,10 +5,9 @@ import com.doto.doto.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +17,12 @@ public class TodoController {
   private final TodoService todoService;
 
   @GetMapping("/new")
-  public String createForm(Model model) {
-    model.addAttribute("todo", new TodoDTO());
+  public String showForm(@RequestParam(value = "date", required = false) String date, Model model) {
+    TodoDTO todo = new TodoDTO();
+    if (date != null && !date.isEmpty()) {
+      todo.setStartDate(LocalDate.parse(date));  // 날짜를 시작일로 세팅
+    }
+    model.addAttribute("todo", todo);
     return "todo/form";
   }
 
